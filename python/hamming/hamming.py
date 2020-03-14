60b01b5e9084676afef5cc3c6fd62ddf1b00d719
brayan <brayan3288@gmail.com>
2020-03-14 15:34:37 -0500

    twenty two exercises completed

---
+++ python/hamming/hamming.py
@@ -0,0 +1,3 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b): raise ValueError("Length must be equal.")
+    return sum(1 for i in range(0, len(strand_a)) if strand_a[i] != strand_b[i])
