7d8f0328f60fd5125f896103a5902711b6bcded2
brayan <brayan3288@gmail.com>
2020-04-03 20:06:49 -0500

    graind completed

---
+++ python/grains/grains.py
@@ -0,0 +1,8 @@
+def square(number):
+    if not 1 <= number <= 64:
+        raise ValueError('Out of range')
+    return 2 ** (number - 1)
+
+
+def total():
+    return sum(2**(n - 1) for n in range(1, 65))
